86ead90adfb7bd9a82784817eb26fb476eff1cd9
Sarah Proctor <sproctor950@gmail.com>
2019-10-18 18:40:00 -0700

    adds factory bot

--- spec/requests/activites_spec.rb
+++ spec/requests/activites_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ActivitiesController, type: :request do
+  let!(:activity){create(:activity)}
   it 'gets all activites' do
     get '/activities'
     expect(response.status).to eq(200)
   end
   it 'returns activities' do
-    Activity.create!
     get '/activities'
     parsed_response = JSON.parse(response.body)
     expect(parsed_response.length).to eq(1)
